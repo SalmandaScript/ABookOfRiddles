8701ed14d0d8508e27c618505c209b2a8669d121
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2023-11-10 16:06:30 +0200

    Add files via upload

---
+++ riddles.js
@@ -0,0 +1,104 @@
+        function startTime( )
+        {var today=new Date();
+        var h=today.getHours();
+        var m=today.getMinutes();
+        var s=today.getSeconds();
+        m=checkTime(m);
+        s=checkTime(s);
+        document.getElementById( 'txt').innerHTML=h + ":" +m+":" + s;
+        var t=setTimeout(startTime, 500); 
+}
+        function checkTime( i) { 
+        if (i < 10) {i = "0" + i};
+        return i; 
+} 
+
+function salmanda(){
+document.getElementById ("demoi"). style. color ="white";
+
+document.getElementById ("demoi"). style. fontweight ="bold";
+
+document.getElementById ("demoi"). style. fontsize ="30%";
+
+document.getElementById ("demoi"). style. background ="green";
+}
+
+function salmanda2 (){
+document.getElementById ("demoii"). style. color ="white";
+
+document.getElementById ("demoii"). style. fontweight ="bold";
+
+document.getElementById ("demoii"). style. fontsize ="30%";
+
+document.getElementById ("demoii"). style. background ="red";
+}
+
+function salmanda3(){
+document.getElementById ("demoiii"). style. color ="white";
+
+document.getElementById ("demoiii"). style. fontweight ="bold";
+
+document.getElementById ("demoiii"). style. fontsize ="30%";
+
+document.getElementById ("demoiii"). style. background ="red";
+}
+
+function salmanda4(){
+document.getElementById ("demoiv"). style. color ="white";
+
+document.getElementById ("demoiv"). style. fontweight ="bold";
+
+document.getElementById ("demoiv"). style. fontsize ="30%";
+
+document.getElementById ("demoiv"). style. background ="red";
+}
+
+function salmanda5(){
+document.getElementById ("demov"). style. color ="white";
+
+document.getElementById ("demov"). style. fontweight ="bold";
+
+document.getElementById ("demov"). style. fontsize ="30%";
+
+document.getElementById ("demov"). style. background ="green";
+}
+
+function salmanda6(){
+document.getElementById ("demovi"). style. color ="white";
+
+document.getElementById ("demovi"). style. fontweight ="bold";
+
+document.getElementById ("demovi"). style. fontsize ="30%";
+
+document.getElementById ("demovi"). style. background ="red";
+}
+
+function salmanda7(){
+document.getElementById ("demovii"). style. color ="white";
+
+document.getElementById ("demovii"). style. fontweight ="bold";
+
+document.getElementById ("demovii"). style. fontsize ="30%";
+
+document.getElementById ("demovii"). style. background ="red";
+}
+
+function salmanda8(){
+document.getElementById ("demoviii"). style. color ="white";
+
+document.getElementById ("demoviii"). style. fontweight ="bold";
+
+document.getElementById ("demoviii"). style. fontsize ="30%";
+
+document.getElementById ("demoviii"). style. background ="red";
+}
+
+function salmanda9(){
+document.getElementById ("demoviiii"). style. color ="white";
+
+document.getElementById ("demoviiii"). style. fontweight ="bold";
+
+document.getElementById ("demoviiii"). style. fontsize ="30%";
+
+document.getElementById ("demoviiii"). style. background ="green";
+}
